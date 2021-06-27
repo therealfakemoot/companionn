927075963c8105fb4053c468c3b376a3b50b16a2
Nick Dumas <drakonik@gmail.com>
2021-06-27 19:09:05 -0400

    Remove defunct debug, add codex to human name maps

--- cmd/demo/main.go
+++ cmd/demo/main.go
@@ -11,12 +11,14 @@ import (
 
 func main() {
 	var (
-		name   string
-		origin string
+		name    string
+		origin  string
+		nearest bool
 	)
 
 	flag.StringVar(&name, "name", "", "Commander name")
 	flag.StringVar(&origin, "origin", "Sol", "Reference system")
+	flag.BoolVar(&nearest, "nearest", false, "Print only the nearest codex entry")
 
 	flag.Parse()
 
@@ -29,30 +31,22 @@ func main() {
 		log.Fatalf("error fetching challenge report: %s", err)
 	}
 
-	// fmt.Printf("%#v\n", cr["Aleoids"].Remaining())
-	// fmt.Printf("%d\n", len(cr["Aleoids"].Remaining()))
 	hm, err := helper.LoadHierarchy("json/hierarchy.json")
 	if err != nil {
 		log.Fatalf("error loading hierarchy data: %s", err)
 	}
 
-	f := helper.FlattenHierarchy(hm)
-	// log.Printf("%#v\n", f)
-	// for k, v := range f {
-	// log.Printf("%s: %#v\n", k, v)
-	// }
-	// log.Printf("%#v\n", hm["Biology"]["Aleoids"])
+	humanToCodex := helper.FlattenHierarchy(hm)
+	codexToHuman := make(map[string]string)
+	for k, v := range humanToCodex {
+		codexToHuman[v.Name] = k
+	}
 
 	var codexTodo []string
 	for _, v := range cm {
 		for _, challenge := range v.Remaining() {
 			codexTodo = append(codexTodo, challenge)
 		}
-	}
-
-	var codexKeys []string
-	for _, c := range codexTodo {
-		codexKeys = append(codexKeys, f[c].Name)
 	}
 
 	challenges, err := helper.LoadEntries("json/codex.json")
