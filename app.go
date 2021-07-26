6875b0b33addfcb61de4561ffc046cac8813d5c2
Nick Dumas <drakonik@gmail.com>
2021-07-25 22:37:23 -0400

    a very bad skeleton

---
+++ app.go
@@ -0,0 +1,35 @@
+package helper
+
+import (
+	"fmt"
+	// "github.com/sirupsen/logrus"
+)
+
+type System interface {
+	Name() string
+	Coords() []float64
+}
+
+type CoordinateRepository interface {
+	Get(string) []float64
+	Set(string, []float64)
+}
+
+type Helper struct {
+	Name        string
+	Entries     []CodexEntry
+	KeyedCodex  KeyedCodex
+	SystemCache CoordinateRepository
+}
+
+func NewHelper(name, challenges string, cache CoordinateRepository) (*Helper, error) {
+	var nh Helper
+	nh.Name = name
+	entries, err := LoadEntries(challenges)
+	if err != nil {
+		return &nh, fmt.Errorf("couldn't load codex entries: %w", err)
+	}
+	nh.Entries = entries
+
+	return &nh, nil
+}
